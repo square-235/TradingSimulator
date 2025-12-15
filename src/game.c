59d11d5144dc4894cae73abfa553da458181de70
square235 <che13060019723@outlook.com>
2025-12-15 15:17:45 +0800

    暂存

--- src/game.c
+++ src/game.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <sys/select.h>
 #include <unistd.h>
-void update_ui(int resTime,int resPer,int money,int debt,int TotalUnpaidShares){//游戏界面更新
+void update_ui(int resTime,int resPer,int money,int debt,int TotalUnpaidShares,Stock *pool){//游戏界面更新
     /*
      * 游戏界面
      * 定时更新主界面（无论是否操作）
      */
     CLEAR();
     MOVETO(0,0);
-    printf("    ┌──────────────────────────────────────────────────────────────────────────────────────────────────┐\n┌───│正在交易 剩余时间：%3ds 下次刷新：%3ds 当前余额:%7d 总资产:%7d 欠款:%7d 欠股票:%7d│───┐\n│   └──────────────────────────────────────────────────────────────────────────────────────────────────┘   │\n│    id:xxx 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:xxx 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:xxx 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:xxx 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:xxx 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n└──────────────────────────────────────────────────────────────────────────────────────────────────────────┘\n",resTime,resPer,money,money-debt-TotalUnpaidShares,debt,TotalUnpaidShares);
+    printf("    ┌──────────────────────────────────────────────────────────────────────────────────────────────────┐\n┌───│正在交易 剩余时间：%3ds 下次刷新：%3ds 当前余额:%7d 总资产:%7d 欠款:%7d 欠股票:%7d│───┐\n│   └──────────────────────────────────────────────────────────────────────────────────────────────────┘   │\n│    id:%d 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:%d 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:%d 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:%d 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n│    id:%d 当前单价：xxx.xx 持有数量:xxxxx 持有价值:xxxxxxx 欠数量:xxxxx 欠价值:xxxxxxx                   │\n└──────────────────────────────────────────────────────────────────────────────────────────────────────────┘\n",resTime,resPer,money,money-debt-TotalUnpaidShares,debt,TotalUnpaidShares,pool[0].code,pool[1].code,pool[2].code,pool[3].code,pool[4].code);
     printf("可用操作：「1」买入 「2」卖出 「3」借款 「4」借股票 「5」打工 「q」提前结算 「Ctrl+C」退出程序\n");
     printf("说明：一次输入一个字符，输入后按下回车生效 ");
     fflush(stdout);//强制更新缓冲区
@@ -34,7 +34,7 @@ void game(int timelimit,int per){
     struct timeval timeout;
 
     while (1) {
-        update_ui(resTime,resPer,money,debt,TotalUnpaidShares);//参数还没准备完成(还差股票链表)
+        update_ui(resTime,resPer,money,debt,TotalUnpaidShares,&pool[5]);//参数还没准备完成(还差股票链表)
         FD_ZERO(&set);
         FD_SET(0,&set);//0指代的是输入缓冲区
         timeout.tv_sec = 1;timeout.tv_usec = 0;//1s刷新界面
